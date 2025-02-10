4602a1bd563ac8c6b06a2e5f4559cbc82c2fdb4b
Bhavesh Mishra <130285034+bhavesh0907@users.noreply.github.com>
2025-02-11 03:44:56 +0530

    summarization_model.py

---
+++ summarization_model.py
@@ -0,0 +1,6 @@
+from transformers import pipeline
+
+def summarize_article(text):
+    summarizer = pipeline("summarization")
+    summary = summarizer(text, max_length=150, min_length=50, do_sample=False)
+    return summary[0]['summary_text']
